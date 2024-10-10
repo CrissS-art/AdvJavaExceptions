2f1532befd2505c07bbae822e13c30135e954980
Chris Le_Jeune <christophe.lejeune@fms-ea.com>
2024-10-10 10:16:41 +0200

    Ex completed - It is working fine

--- src/AdvEx2Thread.java
+++ src/AdvEx2Thread.java
@@ -1,4 +1,3 @@
-package fr.ldnr.thread;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -8,27 +7,32 @@ public class AdvEx2Thread {
 
   public static void main(String[] args) {
     DateFormat df = new SimpleDateFormat("HH:mm:ss");
-    Thread thread = new Thread(new MonRunnable(1000));
+    Thread thread = new Thread(new MyRunnable(1000));
 
     System.out.println(df.format(new Date()));
 
     thread.start();
   }
 
-  private static class MonRunnable implements Runnable {
+  private static class MyRunnable implements Runnable {
 
-    private long delai;
+    private long delay;
 
-    public MonRunnable(long delai) {
-      this.delai = delai;
+    public MyRunnable(long delay) {
+      this.delay = delay;
     }
 
     @Override
     public void run() {
     	while(true) {
-	      try {
-	        Thread.sleep(delai);
-	        System.out.println("-");
+            try {
+                Thread.sleep(delay);
+                for (int i = 1; i <= 5; i++) {
+                    for (int j = 0; j < (6 - i); j++) { 
+                        System.out.print(i + " - "); 
+                    }
+                    System.out.println(); 
+                }
 	      } catch (InterruptedException e) {
 	        e.printStackTrace();
 	      }
