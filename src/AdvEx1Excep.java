ce8eadae55e6841f19b02949fd9caf700fdf47d4
CrissS-art <christophe.lejeune@fms-ea.com>
2024-10-10 14:54:14 +0200

    Update AdvEx1Excep.java
    deleted comments

--- src/AdvEx1Excep.java
+++ src/AdvEx1Excep.java
@@ -1,10 +1,6 @@
 import java.util.Date;
 
 public class AdvEx1Excep {
-
-		// Exercice 1.1 : Le programme suivant présente des problèmes, trouver lesquels et 
-//		utiliser tous les moyens permettant l’affichage des 2 instructions en 
-//		utilisant notamment le mécanisme d’exception
 		
 		private Date date = null;
 		private Date today = new Date();
